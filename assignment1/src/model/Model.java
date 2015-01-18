cc0fa67967d9d20bbefd59627c1e63d7111e742d
Arturo Garcia <artgarcia87@gmail.com>
2015-01-18 09:48:03 -0600

    added functions to Model

--- assignment1/src/model/Model.java
+++ assignment1/src/model/Model.java
@@ -5,11 +5,28 @@ import main.*;
 
 public class Model {
 
-	private ArrayList<Part> parts = null;
+	private ArrayList<Part> partList = null;
 	
 	public Model(){
 		
-		parts = new ArrayList<Part>();
+		partList = new ArrayList<Part>();
+		
+	}
 	
+	public void addPart(String partNum, String partName, String vendor, int quantity){
+		Part part = new Part(partNum, partName, vendor, quantity);
+		partList.add(part);
+	}
+	
+	public void deletePart(Part part){
+		
+	}
+	
+	public void editPart(Part part){
+		
+	}
+	
+	public ArrayList<Part> getPartList(){
+		return partList;
 	}
 }
